e6342a05cff638d958c772a61c8491455e2f2141
manthansharma17 <manthannaruto10@gmail.com>
2026-07-31 21:49:07 +0530

    Q4 answered

--- part4_complexity.py
+++ part4_complexity.py
@@ -27,5 +27,32 @@ print("\nFor n = 20:")
 print("Single loop runs:", count)
 
 
+# -------------------------
+# Nested Loop
+# -------------------------
 
+print("\nNested Loop:")
+
+count = 0
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(i, j)
+        count = count + 1
+
+print("Nested loop total prints:", count)
+
+
+# -------------------------
+# Nested Loop for n = 10
+# -------------------------
+
+n = 10
+count = 0
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        count = count + 1
+
+print("\nFor n = 10:")
 print("Nested loop runs:", count)
